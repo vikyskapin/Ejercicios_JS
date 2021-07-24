111d72e3814a7cadeeefc622336158c868c2eaac
vikyskapin <43868743+vikyskapin@users.noreply.github.com>
2021-07-24 12:20:32 -0300

    Calculadora de salarios

---
+++ CalculadoraSalarios/calculadora_salarios.js
@@ -0,0 +1,12 @@
+document.querySelector('#calcularSalario-mensual').onclick = function(){
+    const salarioAnual = Number(document.querySelector('#salario-anual').value);
+    const salarioMensual = calcularSalarioMensual(salarioAnual);
+
+    document.querySelector('#salario-mensual').value = salarioMensual;
+
+    return false;
+}
+
+function calcularSalarioMensual(salarioAnual){
+    return salarioAnual / 12;
+}
